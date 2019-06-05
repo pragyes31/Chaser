42cb78f453548155a2accb166c920f0078b0c6d2
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-06 00:55:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+let ranks = [
+  {
+    name:"Albert",
+    rank:2
+  },
+  {
+    name:"Issac",
+    rank:3
+  },
+  {
+    name:"Marie",
+    rank:1
+  },
+  {
+    name:"Galileo",
+    rank:4
+  },
+  {
+    name:"Nikola",
+    rank:8
+  },
+  {
+    name:"Leonardo",
+    rank:6
+  },
+  {
+    name:"Charles",
+    rank:5
+  }
+]
+
+const sortByRank = () =>{
+  
+}
+
+sortByRank();
